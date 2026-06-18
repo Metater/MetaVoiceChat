bb0c92cb1ea1dfed82eb9a2ba36cfbb49a6d1c48
Connor Myers <61102529+Metater@users.noreply.github.com>
2026-06-18 16:21:10 -0400

    Update VcFrequency.cs

--- Assets/MetaVoiceChat/Core/VcFrequency.cs
+++ Assets/MetaVoiceChat/Core/VcFrequency.cs
@@ -1,14 +1,12 @@
 ﻿namespace MetaVoiceChat.Core
 {
-    // Opus supports 12 kHz, but NetEQ doesn't
-
     [System.Serializable]
     public enum VcFrequency : byte
     {
         Hz48000,
         Hz24000,
         Hz16000,
-        //Hz12000,
+        Hz12000,
         Hz8000,
     }
 
@@ -21,7 +19,7 @@
                 VcFrequency.Hz48000 => 48000,
                 VcFrequency.Hz24000 => 24000,
                 VcFrequency.Hz16000 => 16000,
-                //VcFrequency.Hz12000 => 12000,
+                VcFrequency.Hz12000 => 12000,
                 VcFrequency.Hz8000 => 8000,
                 _ => 48000,
             };
@@ -32,7 +30,7 @@
             return frequency == VcFrequency.Hz48000 ||
                    frequency == VcFrequency.Hz24000 ||
                    frequency == VcFrequency.Hz16000 ||
-                   //frequency == VcFrequency.Hz12000 ||
+                   frequency == VcFrequency.Hz12000 ||
                    frequency == VcFrequency.Hz8000;
         }
     }
